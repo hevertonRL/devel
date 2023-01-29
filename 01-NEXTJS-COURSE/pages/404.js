fa08529bc6a12ffbd3c5131c418b715bee42b7e5
Heverton Rodrigues Lopes <heverton_rl@hotmail.com>
2023-01-28 21:00:23 -0300

    Final da aula 8

---
+++ 01-NEXTJS-COURSE/pages/404.js
@@ -0,0 +1,13 @@
+import Link from '../src/components/Link'
+
+export default function Page404(){
+    return(
+        <div>
+            <h1>Você se perdeu e caiu na Página 404 :0</h1>
+
+            <Link href="/">
+                Ir para a Home
+            </Link>
+        </div>
+    )
+}
